8a1cd36a8d4cd0997087ec5a76966f6da10f5c30
Ivan Vergés <ivan@pokecode.net>
2022-09-21 15:16:50 +0200

    add category to new reporting proposal

---
+++ app/commands/decidim/reporting_proposals/create_reporting_proposal.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Decidim
+  module ReportingProposals
+    class CreateReportingProposal < Decidim::Proposals::CreateProposal
+      def create_proposal
+        PaperTrail.request(enabled: false) do
+          @proposal = Decidim.traceability.perform_action!(
+            :create,
+            Decidim::Proposals::Proposal,
+            @current_user,
+            visibility: "public-only"
+          ) do
+            proposal = Decidim::Proposals::Proposal.new(
+              title: {
+                I18n.locale => title_with_hashtags
+              },
+              body: {
+                I18n.locale => body_with_hashtags
+              },
+              category: form.category,
+              component: form.component
+            )
+            proposal.add_coauthor(@current_user, user_group: user_group)
+            proposal.save!
+            proposal
+          end
+        end
+      end
+    end
+  end
+end
--- app/controllers/concerns/decidim/reporting_proposals/proposals_controller_override.rb
+++ app/controllers/concerns/decidim/reporting_proposals/proposals_controller_override.rb
@@ -7,6 +7,35 @@ module Decidim
       extend ActiveSupport::Concern
 
       included do
+        def new
+          enforce_permission_to :create, :proposal
+          @step = :step_1
+          if proposal_draft.present?
+            redirect_to edit_draft_proposal_path(proposal_draft, component_id: proposal_draft.component.id, question_slug: proposal_draft.component.participatory_space.slug)
+          else
+            @form = form(new_proposal_form).from_params(body: translated_proposal_body_template)
+          end
+        end
+
+        def create
+          enforce_permission_to :create, :proposal
+          @step = :step_1
+          @form = form(new_proposal_form).from_params(proposal_creation_params)
+
+          create_proposal_command.call(@form, current_user) do
+            on(:ok) do |proposal|
+              flash[:notice] = I18n.t("proposals.create.success", scope: "decidim")
+
+              redirect_to "#{Decidim::ResourceLocatorPresenter.new(proposal).path}/compare"
+            end
+
+            on(:invalid) do
+              flash.now[:alert] = I18n.t("proposals.create.error", scope: "decidim")
+              render :new
+            end
+          end
+        end
+
         def complete
           enforce_permission_to :edit, :proposal, proposal: @proposal
           @step = :step_3
@@ -15,7 +44,22 @@ module Decidim
 
           @form.attachment = form_attachment_new
 
-          redirect_to "#{Decidim::ResourceLocatorPresenter.new(@proposal).path}/preview" if @form.component.manifest_name == "reporting_proposals"
+          redirect_to "#{Decidim::ResourceLocatorPresenter.new(@proposal).path}/preview" if reporting_proposals?
+        end
+
+        private
+
+        def new_proposal_form
+          reporting_proposals? ? Decidim::Proposals::ProposalForm : Decidim::Proposals::ProposalWizardCreateStepForm
+        end
+
+        def create_proposal_command
+          reporting_proposals? ? CreateReportingProposal : Decidim::Proposals::CreateProposal
+        end
+
+        def reporting_proposals?
+          component = current_component || @form.component
+          component.manifest_name == "reporting_proposals"
         end
       end
     end
