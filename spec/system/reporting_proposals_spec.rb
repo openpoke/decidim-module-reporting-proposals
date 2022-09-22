ab8eb2766291d330211174169cafc36df2b12204
Ivan Vergés <ivan@pokecode.net>
2022-09-22 19:36:02 +0200

    test MAP checkbox

--- spec/system/reporting_proposals_spec.rb
+++ spec/system/reporting_proposals_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "spec_helper"
+require "system/shared/proposals_steps_examples"
 
 describe "Reporting proposals overrides", type: :system do
   include_context "with a component"
@@ -58,130 +59,6 @@ describe "Reporting proposals overrides", type: :system do
     end
   end
 
-  shared_examples "3 steps" do
-    it "sidebar does not have the complete step" do
-      click_link "New proposal"
-
-      expect(page).to have_content("Step 1 of 3")
-      within ".wizard__steps" do
-        expect(page).not_to have_content("Complete")
-      end
-    end
-
-    it "redirects to the publish step" do
-      click_link "New proposal"
-
-      fill_proposal
-
-      expect(page).to have_content(proposal_title)
-      expect(page).to have_content(user.name)
-      expect(page).to have_content(proposal_body)
-      expect(page).to have_content(translated(proposal_category.name))
-
-      expect(page).to have_selector("button", text: "Publish")
-
-      expect(page).to have_selector("a", text: "Modify the proposal")
-    end
-
-    it "publishes the proposal" do
-      click_link "New proposal"
-
-      fill_proposal
-
-      click_button "Publish"
-
-      expect(page).to have_content("successfully published")
-
-      expect(page).to have_content(proposal_title)
-      body = translated(proposal.body)
-      expect(body).to have_content(proposal_body)
-      expect(proposal.category).to eq(category)
-      expect(proposal.address).to eq(address)
-      expect(proposal.latitude).to eq(latitude)
-      expect(proposal.longitude).to eq(longitude)
-      expect(body).to have_content("HashtagAuto1")
-      expect(body).to have_content("HashtagAuto2")
-      expect(body).to have_content("HashtagSuggested1")
-      expect(body).not_to have_content("HashtagSuggested2")
-      expect(proposal.identities.first).to eq(user_group)
-      expect(proposal.scope).to eq(scope)
-    end
-
-    it "modifies the proposal" do
-      click_link "New proposal"
-
-      fill_proposal
-
-      click_link "Modify the proposal"
-
-      complete_proposal
-
-      click_button "Publish"
-
-      expect(page).to have_content(proposal_title)
-      expect(translated(proposal.body)).to have_content(proposal_body)
-      expect(proposal.category).to eq(another_category)
-    end
-
-    it "stores no address if checked" do
-      click_link "New proposal"
-
-      fill_proposal(skip_address: true, skip_group: true, skip_scope: true)
-
-      click_button "Publish"
-
-      expect(page).to have_content("successfully published")
-
-      expect(page).to have_content(proposal_title)
-      expect(translated(proposal.body)).to have_content(proposal_body)
-      expect(proposal.category).to eq(category)
-      expect(proposal.identities.first).to eq(user)
-      expect(proposal.scope).to be_nil
-      expect(proposal.address).to be_nil
-      expect(proposal.latitude).to be_nil
-      expect(proposal.longitude).to be_nil
-    end
-  end
-
-  shared_examples "4 steps" do
-    it "sidebar has the complete step" do
-      click_link "New proposal"
-
-      expect(page).to have_content("Step 1 of 4")
-      within ".wizard__steps" do
-        expect(page).to have_content("Complete")
-      end
-    end
-
-    it "redirects to the complete step" do
-      click_link "New proposal"
-
-      fill_proposal(extra_fields: false)
-
-      within ".section-heading" do
-        expect(page).to have_content("COMPLETE YOUR PROPOSAL")
-      end
-
-      expect(page).to have_css(".edit_proposal")
-    end
-
-    it "publishes the proposal" do
-      click_link "New proposal"
-
-      fill_proposal(extra_fields: false)
-      expect(proposal.identities.first).to eq(user)
-
-      complete_proposal
-
-      click_button "Publish"
-
-      expect(page).to have_content(proposal_title)
-      expect(translated(proposal.body)).to eq(proposal_body)
-      expect(proposal.category).to eq(another_category)
-      expect(proposal.identities.first).to eq(user_group)
-    end
-  end
-
   context "when creating a new proposal", :serves_geocoding_autocomplete do
     before do
       visit_component
@@ -190,9 +67,14 @@ describe "Reporting proposals overrides", type: :system do
     it_behaves_like "3 steps"
   end
 
-  context "and component is a normal proposal" do
+  context "and component is a normal proposal", :serves_geocoding_autocomplete do
     let(:manifest_name) { "proposals" }
-    let!(:component) { create(:proposal_component, :with_creation_enabled, participatory_space: participatory_process) }
+    let!(:component) do
+      create(:proposal_component,
+             :with_creation_enabled,
+             :with_geocoding_enabled,
+             participatory_space: participatory_process)
+    end
 
     before do
       visit_component
