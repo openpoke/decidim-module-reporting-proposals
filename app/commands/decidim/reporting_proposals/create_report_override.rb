adfd8c5db167ee4d0dd27040ee3888808a9cdfc7
antopalidi <t.opalidisanna@gmail.com>
2022-09-22 22:54:24 +0200

    fix: change method to override

--- app/commands/decidim/reporting_proposals/create_report_override.rb
+++ app/commands/decidim/reporting_proposals/create_report_override.rb
@@ -6,11 +6,11 @@ module Decidim
       extend ActiveSupport::Concern
 
       included do
-        def send_report_notification_to_moderators
+        def send_hide_notification_to_moderators
           participatory_space_moderators.each do |moderator|
             next unless moderator.email_on_moderations
 
-            ReportedMailer.report(moderator, @report).deliver_later unless @current_user.admin?
+            ReportedMailer.hide(moderator, @report).deliver_later unless @current_user.admin?
           end
         end
       end
