f642fa02aa7d36cf366d7d4ecb07c2288566e911
Ivan Vergés <ivan@pokecode.net>
2022-09-28 17:23:24 +0200

    js linting

--- app/packs/src/decidim/reporting_proposals/reverse_geocoding.js
+++ app/packs/src/decidim/reporting_proposals/reverse_geocoding.js
@@ -1,56 +1,57 @@
 $(() => {
-	const info = ($target, msg) => {
-		$label = $target.closest("label")
-		$label.find(".form-error").remove();
-		if(msg) {
-			$('<span class="form-error"/>').text(msg).appendTo($label).show();
-		}
-	};
+  const info = ($target, msg) => {
+    const $label = $target.closest("label")
+    $label.find(".form-error").remove();
+    if (msg) {
+      $('<span class="form-error"/>').text(msg).appendTo($label).show();
+    }
+  };
 
-	const setLocating = ($button, enable) => {
-		if(enable) {
-			$button.addClass("loading-spinner");
-  		$button.attr("disabled", true);
-		} else {
-			$button.removeClass("loading-spinner");
-  		$button.attr("disabled", false);
-		}
-	}
+  const setLocating = ($button, enable) => {
+    if (enable) {
+      $button.addClass("loading-spinner");
+      $button.attr("disabled", true);
+    } else {
+      $button.removeClass("loading-spinner");
+      $button.attr("disabled", false);
+    }
+  }
 
   $(".user-device-location button").on("click", (e) => {
-  	const $this = $(e.target);
-  	if($this.is(":disabled")) return;
+    const $this = $(e.target);
+    if ($this.is(":disabled")) {
+      return;
+    }
 
-  	const $input = $("#" + e.target.dataset.input);
-  	const $latitude = $("#" + e.target.dataset.latitude);
-  	const $longitude = $("#" + e.target.dataset.longitude);
-  	const errorNoLocation = e.target.dataset.errorNoLocation;
-  	const errorUnsupported = e.target.dataset.errorUnsupported;
-  	const url = e.target.dataset.url;
-  	console.log("locator", $input, $latitude, $longitude, url,e);
+    const $input = $(`#${e.target.dataset.input}`);
+    const $latitude = $(`#${e.target.dataset.latitude}`);
+    const $longitude = $(`#${e.target.dataset.longitude}`);
+    const errorNoLocation = e.target.dataset.errorNoLocation;
+    const errorUnsupported = e.target.dataset.errorUnsupported;
+    const url = e.target.dataset.url;
+    console.log("locator", $input, $latitude, $longitude, url, e);
 
-  	if (!navigator.geolocation) {
-	  	info($input, errorUnsupported)
-	  } else {
-	  	setLocating($this, true);
-	  	navigator.geolocation.getCurrentPosition((position) => {
-	  		const coordinates  = [position.coords.latitude, position.coords.longitude];
+    if (navigator.geolocation) {
+      setLocating($this, true);
+      navigator.geolocation.getCurrentPosition((position) => {
+        const coordinates  = [position.coords.latitude, position.coords.longitude];
         $input.trigger(
           "geocoder-suggest-coordinates.decidim",
           [coordinates]
         );
-    		// reverse geolocation
-	  		$.post(url, { latitude: coordinates[0], longitude: coordinates[1] }, (data) => {
-	  			$input.val(data.address)
-	  		})
-	  		setLocating($this, false);
-	  	}, (e) => { 
-	  		console.log("error", e)
-	  		info($input, `${errorNoLocation} ${e.message}`);
-	  		$this.attr("disabled", false);
-	  }, {
-	  	enableHighAccuracy: true
-	  });
-	  }
+        // reverse geolocation
+        $.post(url, { latitude: coordinates[0], longitude: coordinates[1] }, (data) => {
+          $input.val(data.address)
+        })
+        setLocating($this, false);
+      }, (evt) => { 
+        info($input, `${errorNoLocation} ${evt.message}`);
+        $this.attr("disabled", false);
+      }, {
+        enableHighAccuracy: true
+      });
+    } else {
+      info($input, errorUnsupported);
+    }
   });
 });
