d5183b36dfc4a5486b2e3265ad1199ec0ab8cc94
Ivan Vergés <ivan@pokecode.net>
2022-09-28 15:21:08 +0200

    add permissions to proposals controller

--- app/controllers/decidim/reporting_proposals/admin/application_controller.rb
+++ app/controllers/decidim/reporting_proposals/admin/application_controller.rb
@@ -9,6 +9,9 @@ module Decidim
       # Note that it inherits from `Decidim::Admin::Components::BaseController`, which
       # override its layout and provide all kinds of useful methods.
       class ApplicationController < Decidim::Admin::ApplicationController
+        def permission_class_chain
+          [::Decidim::ReportingProposals::Admin::Permissions] + super
+        end
       end
     end
   end
--- app/controllers/decidim/reporting_proposals/admin/proposals_controller.rb
+++ app/controllers/decidim/reporting_proposals/admin/proposals_controller.rb
@@ -6,6 +6,8 @@ module Decidim
       class ProposalsController < Admin::ApplicationController
         # TODO: permissions
         def hide_proposal
+          enforce_permission_to :hide, :resource, resource: proposal
+
           Decidim::Admin::HideResource.call(proposal, current_user) do
             on(:ok) do
               flash[:notice] = I18n.t("reportable.hide.success", scope: "decidim.moderations.admin")
