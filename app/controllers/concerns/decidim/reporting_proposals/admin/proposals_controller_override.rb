1a05a7202070ba4ae93cb128d55354509d8b3ed6
antopalidi <t.opalidisanna@gmail.com>
2022-10-05 10:27:15 +0200

    add photo_form for reporting proposals only

--- app/controllers/concerns/decidim/reporting_proposals/admin/proposals_controller_override.rb
+++ app/controllers/concerns/decidim/reporting_proposals/admin/proposals_controller_override.rb
@@ -8,10 +8,18 @@ module Decidim
         extend ActiveSupport::Concern
 
         included do
+          helper_method :reporting_proposal?, :proposals, :query, :form_presenter, :proposal, :proposal_ids
           def show
             @notes_form = form(Decidim::Proposals::Admin::ProposalNoteForm).instance
             @answer_form = form(Decidim::Proposals::Admin::ProposalAnswerForm).from_model(proposal)
             @photo_form = form(Decidim::ReportingProposals::Admin::ProposalPhotoForm).instance
+          end
+
+          private
+
+          def reporting_proposal?
+            component = current_component || @photo_form.component
+            component.manifest_name == "reporting_proposals"
           end
         end
       end
