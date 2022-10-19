e7648fcb939eaf1f57306be4f72c0d5ca329418d
Ivan Vergés <ivan@pokecode.net>
2022-10-19 18:30:43 +0200

    Replace ERB overrides with Deface (#21)
    * replace new proposals override with deface
    
    * move partial to reporting_proposals namespace
    
    * move photo gallery & form to deface overrides
    
    * move proposal admin index hide action
    
    * force update

--- lib/decidim/reporting_proposals/engine.rb
+++ lib/decidim/reporting_proposals/engine.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "deface"
+
 module Decidim
   module ReportingProposals
     # This is the engine that runs on the public interface of decidim-ReportingProposals.
