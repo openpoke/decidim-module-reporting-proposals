28b7f1edcca5eebf9c5eff7e53c9ac357f52e9a7
antopalidi <t.opalidisanna@gmail.com>
2022-10-20 16:01:04 +0200

    add test

--- spec/system/additional_buttons_spec.rb
+++ spec/system/additional_buttons_spec.rb
@@ -4,38 +4,58 @@ require "spec_helper"
 
 describe "Additional button", type: :system do
   include_context "with a component"
+
   let(:manifest_name) { "reporting_proposals" }
-  let!(:component) do
-    create(:reporting_proposals_component,
-           participatory_space: participatory_process,
-           settings: {
-             additional_buttons_show: additional_buttons_show,
-             additional_button_text: { en: "My button" },
-             additional_button_link: "https://#{organization.host}/processes/onion-dynamic/f/20/"
-           })
-  end
   let!(:participatory_process) { create :participatory_process, :published }
   let!(:organization) { participatory_process.organization }
 
-  before do
-    switch_to_host(organization.host)
-    visit_component
-  end
+  describe "reporting_proposals_component" do
+    let(:component) do
+      create(:reporting_proposals_component,
+             participatory_space: participatory_process,
+             settings: {
+               additional_buttons_show: additional_buttons_show,
+               additional_button_text: { en: "My button" },
+               additional_button_link: "https://#{organization.host}/processes/onion-dynamic/f/20/"
+             })
+    end
 
-  context "when the component does not have the additional button customized" do
-    let(:additional_buttons_show) { false }
+    before do
+      switch_to_host(organization.host)
+      visit_component
+    end
 
-    it "does not have an additional button" do
-      expect(page).not_to have_content("My button")
+    context "when the component does not have the additional button customized" do
+      let(:additional_buttons_show) { false }
+
+      it "does not have an additional button" do
+        expect(page).not_to have_content("My button")
+      end
+    end
+
+    context "when the component has the additional button customized" do
+      let(:additional_buttons_show) { true }
+
+      it "has an additional button" do
+        expect(page).to have_content("My button")
+        expect(page).to have_css("a[href='https://#{organization.host}/processes/onion-dynamic/f/20/']")
+      end
     end
   end
 
-  context "when the component has the additional button customized" do
-    let(:additional_buttons_show) { true }
+  describe "proposal component" do
+    let(:component) { create(:proposal_component, participatory_space: participatory_process) }
 
-    it "has an additional button" do
-      expect(page).to have_content("My button")
-      expect(page).to have_css("a[href='https://#{organization.host}/processes/onion-dynamic/f/20/']")
+    context "when the component has the additional button customized" do
+      let(:additional_buttons_show) { true }
+
+      before do
+        visit_component
+      end
+
+      it "does not have an additional button" do
+        expect(page).not_to have_content("My button")
+      end
     end
   end
 end
