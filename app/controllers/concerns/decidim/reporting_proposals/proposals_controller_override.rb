0780d11d1c3125280db0841553dd3a40207b6740
antopalidi <t.opalidisanna@gmail.com>
2022-10-26 00:24:50 +0200

    remove the method edit_form

--- app/controllers/concerns/decidim/reporting_proposals/proposals_controller_override.rb
+++ app/controllers/concerns/decidim/reporting_proposals/proposals_controller_override.rb
@@ -49,16 +49,9 @@ module Decidim
           redirect_to "#{Decidim::ResourceLocatorPresenter.new(@proposal).path}/preview" if reporting_proposal?
         end
 
-        def edit_form
-          form_attachment_model = form(AttachmentForm).from_model(@proposal.attachments.first)
-          @form = form_reporting_proposals_model
-          @form.attachment = form_attachment_model
-          @form
-        end
-
         private
 
-        def form_reporting_proposals_model
+        def form_proposal_model
           reporting_proposal? ? form(Decidim::ReportingProposals::ProposalForm).from_model(@proposal) : form(Decidim::Proposals::ProposalForm).from_model(@proposal)
         end
 
