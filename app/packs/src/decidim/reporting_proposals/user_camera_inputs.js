5bca969ce5db46c43a35c7ebdb9a3fa5ff44f5ef
antopalidi <t.opalidisanna@gmail.com>
2022-10-25 10:08:30 +0200

    fix: remove errors when has_address is checked

--- app/packs/src/decidim/reporting_proposals/user_camera_inputs.js
+++ app/packs/src/decidim/reporting_proposals/user_camera_inputs.js
@@ -4,6 +4,8 @@ $(() => {
     const $inputField = $input.closest(".row.column");
     const $button = $inputField.find("button:first");
     const $checkbox = $inputField.find("input:checkbox[name$='[has_no_image]']");
+    const $formError = $("span.form-error")
+    const $labelInput = $("label[for='proposal_add_photos']")
 
     $input.attr("accept", "image/*");
 
@@ -17,7 +19,10 @@ $(() => {
       const toggleInput = () => {
         if ($checkbox[0].checked) {
           $input.prop("disabled", true);
+          $input.removeClass("is-invalid-input");
           $button.prop("disabled", true);
+          $formError.removeClass("is-visible")
+          $labelInput.removeClass("is-invalid-label")
         } else {
           $input.prop("disabled", false);
           $button.prop("disabled", false);
