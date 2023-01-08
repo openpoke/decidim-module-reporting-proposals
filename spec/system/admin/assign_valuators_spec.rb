4a8687c1671e6e587a4b6ecb02dabd25065c0073
Ivan Vergés <ivan@pokecode.net>
2023-01-08 22:51:14 +0100

    adapt to branch valuators

--- spec/system/admin/assign_valuators_spec.rb
+++ spec/system/admin/assign_valuators_spec.rb
@@ -15,7 +15,23 @@ describe "Assign valuators", type: :system do
 
   include_context "when managing a component as an admin"
 
-  context "when admin to assign a validator" do
+  shared_examples "assigns a valuator" do
+    it "assigns the proposals to the valuator" do
+      within "#valuators" do
+        expect(page).not_to have_content(valuator.name)
+      end
+
+      click_button "Assign"
+
+      expect(page).to have_content("Proposals assigned to a valuator successfully")
+
+      within "#valuators" do
+        expect(page).to have_content(valuator.name)
+      end
+    end
+  end
+
+  context "when admin assigns a validator" do
     before do
       visit current_path
 
@@ -25,17 +41,9 @@ describe "Assign valuators", type: :system do
         find("#valuator_role_id").click
         find("option", text: valuator.name).click
       end
-
-      click_button "Assign"
     end
 
-    it "assigns the proposals to the valuator" do
-      expect(page).to have_content("Proposals assigned to a valuator successfully")
-
-      within find("tr", text: translated(proposal.title)) do
-        expect(page).to have_selector("td.valuators-count", text: 1)
-      end
-    end
+    it_behaves_like "assigns a valuator"
   end
 
   context "when a valuator manages assignments" do
@@ -44,8 +52,8 @@ describe "Assign valuators", type: :system do
 
     before do
       switch_to_host(organization.host)
-      login_as valuator, scope: :user
-      create :valuation_assignment, proposal: proposal, valuator_role: valuator_role
+      login_as second_valuator, scope: :user
+      create :valuation_assignment, proposal: proposal, valuator_role: second_valuator_role
 
       visit current_path
       click_link translated(proposal.title)
@@ -55,24 +63,16 @@ describe "Assign valuators", type: :system do
       before do
         within "#js-form-assign-proposal-to-valuator" do
           find("#valuator_role_id").click
-          find("option", text: second_valuator.name).click
+          find("option", text: valuator.name).click
         end
       end
 
-      it "assigns the proposals to the valuator" do
-        click_button "Assign"
-
-        expect(page).to have_content("Proposals assigned to a valuator successfully")
-
-        within find("tr", text: translated(proposal.title)) do
-          expect(page).to have_selector("td.valuators-count", text: 2)
-        end
-      end
+      it_behaves_like "assigns a valuator"
     end
 
     it "can remove only himself from the evaluators" do
       accept_confirm do
-        within find("#valuators li", text: valuator.name) do
+        within find("#valuators li", text: second_valuator.name) do
           find("a.red-icon").click
         end
       end
