edde9fae138f5661481bc8c57316a0ee10c4a77a
Ivan Vergés <ivan@pokecode.net>
2023-01-10 15:18:51 +0100

    fix override tests

--- spec/lib/overrides_spec.rb
+++ spec/lib/overrides_spec.rb
@@ -11,7 +11,7 @@ checksums = [
     files: {
       "/lib/decidim/component_validator.rb" => "2e3eb588a9579b94c21f4f440751d9a9",
       "/lib/decidim/map/autocomplete.rb" => "65df8e58433814e6b35b52c1bff19ed2",
-      "/lib/decidim/form_builder.rb" => "91c55f8197a9e3892e94f5c13ca40905",
+      "/lib/decidim/form_builder.rb" => "3ea9c3c51ea6e5e54d293326a529d27a",
       "/app/commands/decidim/create_report.rb" => "41c95f194f5ccb5ff32efc43644c9ce5",
       "/app/models/decidim/category.rb" => "f3a51a9e8377f8a658a35363ce1ba8c1",
       "/app/models/decidim/participatory_space_role_config/valuator.rb" => "8dbcc7c787a7db6e9cdebccf888ffbdf"
@@ -37,7 +37,7 @@ checksums = [
   {
     package: "decidim-proposals",
     files: {
-      "/app/controllers/decidim/proposals/proposals_controller.rb" => "6f73c3cad3c713358535c92e2ed84f60",
+      "/app/controllers/decidim/proposals/proposals_controller.rb" => "81075a969be2732d21f244eff3c4c56e",
       "/app/helpers/decidim/proposals/proposal_wizard_helper.rb" => "75d875f3323e75273819d526731140da",
       "/app/commands/decidim/proposals/create_proposal.rb" => "8808a4239d750a86edd6e9c309533788",
       "/app/forms/decidim/proposals/proposal_form.rb" => "ec488556b51b8f28c5031552b04844c1",
@@ -45,13 +45,13 @@ checksums = [
       "/app/views/decidim/proposals/proposals/edit.html.erb" => "7071f1e5e10a22e79511875c50ce63be",
       "/app/views/decidim/proposals/proposals/edit_draft.html.erb" => "0b832f705872e1aea3eefbd76ef4f1fa",
       "/app/views/decidim/proposals/proposals/_edit_form_fields.html.erb" => "6893bb3cc0fe99f4de986f829af26d50",
-      "/app/views/decidim/proposals/admin/proposals/_proposal-tr.html.erb" => "e0a162954ca45ab6b3d71e61635eddbd",
+      "/app/views/decidim/proposals/admin/proposals/_proposal-tr.html.erb" => "b1076d75afb140ded26d4837618f2af5",
       "/app/permissions/decidim/proposals/admin/permissions.rb" => "751b15ced553b17b7203f66a0152fb75"
     }
   }
 ]
 
-describe "Overriden files", type: :view do
+describe "Overriden files" do
   checksums.each do |item|
     # rubocop:disable Rails/DynamicFindBy
     spec = ::Gem::Specification.find_by_name(item[:package])
