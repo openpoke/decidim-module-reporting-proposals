bdcc7b168c7af82fb8515fd0495fa13ea362ae1e
Anna Topalidi <t.opalidisanna@gmail.com>
2023-01-12 13:11:46 +0100

    override answer controller and spec

---
+++ app/controllers/concerns/decidim/reporting_proposals/admin/proposal_answers_controller_override.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module Decidim
+  module ReportingProposals
+    module Admin
+      # Exposes the proposal resource so users can view and create them.
+      module ProposalAnswersControllerOverride
+        extend ActiveSupport::Concern
+
+        included do
+          helper Decidim::Proposals::Admin::ProposalBulkActionsHelper
+          def update
+            enforce_permission_to :create, :proposal_answer, proposal: proposal
+
+            @notes_form = form(Decidim::Proposals::Admin::ProposalNoteForm).instance
+            @answer_form = form(Decidim::Proposals::Admin::ProposalAnswerForm).from_params(params)
+            @photo_form = form(Decidim::ReportingProposals::Admin::ProposalPhotoForm).instance
+
+            Decidim::Proposals::Admin::AnswerProposal.call(@answer_form, proposal) do
+              on(:ok) do
+                flash[:notice] = I18n.t("proposals.answer.success", scope: "decidim.proposals.admin")
+                redirect_to proposals_path
+              end
+
+              on(:invalid) do
+                flash.keep[:alert] = I18n.t("proposals.answer.invalid", scope: "decidim.proposals.admin")
+                render template: "decidim/proposals/admin/proposals/show"
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
