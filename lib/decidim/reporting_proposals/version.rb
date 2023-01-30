8e8effe3d37dd0a0ed943e7f2f469418c3a8174b
Ivan Vergés <ivan@pokecode.net>
2023-01-30 14:31:02 +0100

    show the name of the valuator in the admin

--- lib/decidim/reporting_proposals/version.rb
+++ lib/decidim/reporting_proposals/version.rb
@@ -6,7 +6,13 @@ module Decidim
     # DECIDIM_VERSION = "0.26.5"
     # Provisional until
     #   https://github.com/openpoke/decidim/pull/23
+    #   https://github.com/openpoke/decidim/pull/24
+    #   https://github.com/openpoke/decidim/pull/29
+    #   https://github.com/openpoke/decidim/pull/31
     #   https://github.com/openpoke/decidim/pull/37
+    #   https://github.com/openpoke/decidim/pull/39
+    #   https://github.com/openpoke/decidim/pull/43
+    #   https://github.com/openpoke/decidim/pull/52
     # are merged on upstream
     DECIDIM_VERSION = { github: "openpoke/decidim", branch: "0.26-lucerne" }.freeze
 
