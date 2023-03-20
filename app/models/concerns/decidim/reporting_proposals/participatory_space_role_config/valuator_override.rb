ecb94996d617f51e8c53c061414fef5239975ca5
Ivan Vergés <ivan@pokecode.net>
2023-03-20 12:34:59 +0100

    add component permission to valuators dynamically

--- app/models/concerns/decidim/reporting_proposals/participatory_space_role_config/valuator_override.rb
+++ app/models/concerns/decidim/reporting_proposals/participatory_space_role_config/valuator_override.rb
@@ -7,8 +7,11 @@ module Decidim
         extend ActiveSupport::Concern
 
         included do
+          # store the current value class-wide so it is compatible with other module adding accepted_components elements
+          @@currently_accepted_components = self.new(nil).accepted_components
+
           def accepted_components
-            [:proposals, :reporting_proposals]
+            @accepted_components ||= @@currently_accepted_components + [:reporting_proposals]
           end
         end
       end
