e2ea8e92d31bb350775388f84f12d97c3684ca43
Ivan Vergés <ivan@pokecode.net>
2023-03-20 13:08:49 +0100

    Update valuator_override.rb

--- app/models/concerns/decidim/reporting_proposals/participatory_space_role_config/valuator_override.rb
+++ app/models/concerns/decidim/reporting_proposals/participatory_space_role_config/valuator_override.rb
@@ -8,7 +8,7 @@ module Decidim
 
         included do
           # store the current value class-wide so it is compatible with other module adding accepted_components elements
-          @@currently_accepted_components = self.new(nil).accepted_components
+          @@currently_accepted_components = new(nil).accepted_components
 
           def accepted_components
             @accepted_components ||= @@currently_accepted_components + [:reporting_proposals]
