466f23c3cb5f9101488a4558f5ce429f61f4770c
Ivan Vergés <ivan@pokecode.net>
2023-03-21 13:07:58 +0100

    add component permission to valuators dynamically (#59)
    * add component permission to valuators dynamically
    
    * Update valuator_override.rb
    
    * fix & add test
    
    * ensure no collisions in alias names
    
    * update readme
    
    * update readme

--- app/models/concerns/decidim/reporting_proposals/participatory_space_role_config/valuator_override.rb
+++ app/models/concerns/decidim/reporting_proposals/participatory_space_role_config/valuator_override.rb
@@ -7,8 +7,11 @@ module Decidim
         extend ActiveSupport::Concern
 
         included do
+          # it is important to ensure that the aliased method name is unique in case of other modules are doing the same
+          alias_method :decidim_reporting_proposals_original_accepted_components, :accepted_components
+
           def accepted_components
-            [:proposals, :reporting_proposals]
+            decidim_reporting_proposals_original_accepted_components + [:reporting_proposals]
           end
         end
       end
