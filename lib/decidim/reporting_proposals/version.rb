bf801eb899ada757697a866434749c6b4d353336
Ivan Vergés <ivan@pokecode.net>
2023-06-27 19:36:16 +0200

    patch babel.config.json temporarly

--- lib/decidim/reporting_proposals/version.rb
+++ lib/decidim/reporting_proposals/version.rb
@@ -5,6 +5,6 @@ module Decidim
     VERSION = "0.5.0"
     DECIDIM_VERSION = "0.27.3"
 
-    COMPAT_DECIDIM_VERSION = [">= 0.26.0", "< 0.28"].freeze
+    COMPAT_DECIDIM_VERSION = [">= 0.27.0", "< 0.28"].freeze
   end
 end
