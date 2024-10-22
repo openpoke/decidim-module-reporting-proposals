bf013d80a972b4cc2d06f1c3798244514f9dc4bc
Ivan Vergés <ivan@pokecode.net>
2024-10-22 13:58:07 +0200

    fix override distance

--- lib/decidim/reporting_proposals/version.rb
+++ lib/decidim/reporting_proposals/version.rb
@@ -2,7 +2,7 @@
 
 module Decidim
   module ReportingProposals
-    VERSION = "0.6.1"
+    VERSION = "0.6.2"
     DECIDIM_VERSION = "0.28.4"
     COMPAT_DECIDIM_VERSION = [">= 0.28.1", "< 0.29"].freeze
   end
