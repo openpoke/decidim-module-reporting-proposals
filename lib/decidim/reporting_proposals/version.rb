4b189e27b68bb88845acaa542952bbeaee57179a
Ivan Vergés <ivan@pokecode.net>
2025-12-10 10:43:27 +0100

    update version

--- lib/decidim/reporting_proposals/version.rb
+++ lib/decidim/reporting_proposals/version.rb
@@ -2,8 +2,8 @@
 
 module Decidim
   module ReportingProposals
-    VERSION = "0.7.2"
-    DECIDIM_VERSION = "0.29.4"
+    VERSION = "0.7.3"
+    DECIDIM_VERSION = "0.29.7"
     COMPAT_DECIDIM_VERSION = [">= 0.29.3", "< 0.30"].freeze
   end
 end
